3552859db8b6f1887f8da930e2f5c1b204764759
Sumit saurav <sumitsaurav2107@gmail.com>
2025-06-04 18:38:58 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import Dashbord from "./Component/Dashbord/Dashbord";
+import Dashbord from "./Component/Dashbord/Dashbord";
 
-import Dashbord from "../src/MainComponent/Tax/TaxForm";
+// import Dashbord from "../src/MainComponent/Inventory/Aisles/AislesForm";
 
 const App = () => {
   return (
