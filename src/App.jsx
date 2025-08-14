93a05e5fa98fa060870286bc4e1b7c40f1f7bf27
Sumit saurav <sumitsaurav2107@gmail.com>
2025-08-14 13:48:30 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 //
-// import Dashbord from "./Component/Dashbord/Dashbord";
+import Dashbord from "./Component/Dashbord/Dashbord";
 //
 // import Dashbord from "../src/MainComponent/Sale/SaleMaster/SalePage";
-import Dashbord from "../src/MainComponent/Sale/SaleMaster/SalePage";
+// import Dashbord from "../src/MainComponent/Sale/SaleMaster/SalePage";
 const App = () => {
   return (
     <div>
