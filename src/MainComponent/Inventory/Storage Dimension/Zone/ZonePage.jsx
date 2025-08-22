219fbdf916b70945754a5e508fb94a09a1744799
Sumit saurav <sumitsaurav2107@gmail.com>
2025-08-22 15:34:07 +0530

    22 aug

--- src/MainComponent/Inventory/Storage Dimension/Zone/ZonePage.jsx
+++ src/MainComponent/Inventory/Storage Dimension/Zone/ZonePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ZoneList from "./ZoneList";
+import ZoneList from "./ZonList";
 import ZoneForm from "./ZoneForm";
 // import ZoneViewPage from "./ZoneViewPagee";
 
