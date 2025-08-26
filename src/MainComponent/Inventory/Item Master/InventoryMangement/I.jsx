270b262229e116850dc17a098b5a6395a1568050
Sumit saurav <sumitsaurav2107@gmail.com>
2025-08-26 14:22:21 +0530

    bin

--- src/MainComponent/Inventory/Item Master/InventoryMangement/I.jsx
+++ src/MainComponent/Inventory/Item Master/InventoryMangement/I.jsx
@@ -9,6 +9,7 @@ const initialForm = {
   company: localStorage.getItem("selectedCompany") || "",
 };
 import ItemMasterPage from "../Item/ItemPage";
+import BinPage from "../../Storage Dimension/Bin/BinPage";
 import ProductDimColor from "../../Product Dimension/Color/ProductDimColorPage";
 import ProductDimConfig from "../../Product Dimension/Configuration/ProductDimConfPage";
 import ProductDimVersion from "../../Product Dimension/ProductDimVersion/ProductDimVersionPage";
@@ -45,6 +46,9 @@ const I = () => {
       ),
       [PAGE.COLOR]: (
         <ProductDimColor companies={companies} form={form} setForm={setForm} />
+      ),
+      [PAGE.BIN]: (
+        <BinPage companies={companies} form={form} setForm={setForm} />
       ),
       [PAGE.CONFIG]: (
         <ProductDimConfig companies={companies} form={form} setForm={setForm} />
