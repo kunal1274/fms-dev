7cba2b22ed6b6e726058a1136e8b31a55e9d8d2e
Sumit saurav <sumitsaurav2107@gmail.com>
2025-08-26 12:52:27 +0530

    . repet

--- src/Component/Main/Main.jsx
+++ src/Component/Main/Main.jsx
@@ -3,7 +3,7 @@ import CustomerList from "../../MainComponent/Customer/CustomerPage";
 import CompanyPage from "../../MainComponent/Company/CompanyPage";
 import SalePage from "../../MainComponent/Sale/Sale/SaleManegment";
 import VendorPage from "../../MainComponent/Vendor/VendorPage";
-// import InventoryManegment from "../../MainComponent/Inventory/Item Master/Inventory Mangement/InventoryManegment";
+import InventoryManegment from "../../MainComponent/Inventory/Item Master/Inventory Mangement/InventoryManegment";
 import PurchasePage from "../../MainComponent/Purchase/Purchase/PurchaseManegment";
 import TaxManegment from "../../MainComponent/Tax/TaxMangement/TaxManegment";
 import BankManegment from "../../MainComponent/Bank/BankMangement/BankManegment";
