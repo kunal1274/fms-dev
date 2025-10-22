6216b06c91ee37cd7380ae0b62a7730f4c612b31
kunal1274 <kunal1274@gmail.com>
2025-10-22 12:20:59 +0530

    Delete context directory

--- context/CompanyContext.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const CompanyContext = React.createContext({
-  form: { company: "" },
-  setForm: () => {},
-  companies: [],
-});
-
-export default CompanyContext;
